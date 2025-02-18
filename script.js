3d4d9eed7a3673ee77eb8af336432c74244c2181
AvivKravzov <aviv.kravzov@gmail.com>
2025-02-18 17:48:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let slides = document.querySelectorAll(".slide");
 
     function checkSlides() {
-        slides.forEach(slide => {
+        slides.forEach((slide, index) => {
             let slidePosition = slide.getBoundingClientRect().top;
             let screenPosition = window.innerHeight / 1.3;
+
+            console.log(`שקופית ${index + 1}: ${slidePosition} - צריך להיות מתחת ל-${screenPosition}`);
 
             if (slidePosition < screenPosition) {
                 slide.classList.add("visible");
